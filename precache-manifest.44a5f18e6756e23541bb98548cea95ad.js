5f9075827703535b179df8dde633b32b21cc672e
codewilling <codewilling@gmail.com>
2020-10-19 20:08:30 -0700

    Updates

---
+++ precache-manifest.44a5f18e6756e23541bb98548cea95ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9199345047146d14a116f617efeffce4",
+    "url": "/portfolio2020/index.html"
+  },
+  {
+    "revision": "a8ce37284bdaedbdf641",
+    "url": "/portfolio2020/static/css/main.6f970488.chunk.css"
+  },
+  {
+    "revision": "d6558debbc857f5d7896",
+    "url": "/portfolio2020/static/js/2.f2489509.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio2020/static/js/2.f2489509.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8ce37284bdaedbdf641",
+    "url": "/portfolio2020/static/js/main.12b9d4cc.chunk.js"
+  },
+  {
+    "revision": "59ddd8522bdc6577de50",
+    "url": "/portfolio2020/static/js/runtime-main.1118c555.js"
+  },
+  {
+    "revision": "b0f14771d8d7365277983c552cffca4f",
+    "url": "/portfolio2020/static/media/Tents.b0f14771.png"
+  }
+]);
